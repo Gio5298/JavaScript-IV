5c00305080e402c6f13229f1de2a158d42c5e3ec
Regino P <regino5298@gmail.com>
2019-10-17 13:06:43 -0700

    lambda-classes, added Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(attrs){
+        this.name = attrs.name,
+        this.age = attrs.age,
+        this.location = attrs.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
